d4615066732d67b1d64613f2f3a74f91c6bb7c39
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2018-09-25 14:32:45 -0300

    .

--- public/modules/pfcg/controller/UserController.class.js
+++ public/modules/pfcg/controller/UserController.class.js
@@ -1,10 +1,22 @@
+var ejs = require('ejs');
+
 var method = UserController.prototype;
 
 function UserController(){
 }
 
 UserController.actionHome = function(){
-	global.res.end("OK");
+	/*
+	var html = ejs.render("<%= user.name %>", {
+	    user: { name: 'Vinicius' }
+	});
+	*/
+	
+	data = "";
+	options = {user: { name: 'Vinicius' }};
+	ejs.renderFile("./modules/pfcg/view/user-requestLogin.ejs", options, function(err, str){
+		global.res.end(str);
+	});
 }
 
 module.exports = UserController;
